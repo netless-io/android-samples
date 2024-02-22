4a0839b308a7170a231cb5774b67979cd8f58314
fenglibin <fenglibin@agora.io>
2024-02-22 18:26:41 +0800

    update whiteboard

--- WhiteboardCompose/app/src/main/java/io/agora/board/samples/MainActivity.kt
+++ WhiteboardCompose/app/src/main/java/io/agora/board/samples/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
@@ -23,14 +26,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.herewhite.sdk.domain.MemberState
 import io.agora.board.samples.ui.theme.WhiteboardComposeTheme
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WhiteboardComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -42,55 +46,87 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 private fun MainScreen() {
-
-    var showWhiteboard by rememberSaveable { mutableStateOf(false) }
-    val whiteboardState = rememberWhiteboardState()
     val coroutineScope = rememberCoroutineScope()
+    val whiteboardState = rememberWhiteboardState()
+    var whiteboardShown by rememberSaveable { mutableStateOf(true) }
+
+    // 模拟数据获取
+    LaunchedEffect(true) {
+        delay(2000)
+        whiteboardState.whiteboardProperties = WhiteboardProperties(
+            appId = Constants.SAMPLE_APP_ID,
+            roomUuid = Constants.SAMPLE_ROOM_UUID,
+            roomToken = Constants.SAMPLE_ROOM_TOKEN,
+            uniqueUid = Constants.SAMPLE_UNIQUE_UID,
+            region = Constants.SAMPLE_REGION,
+        )
+    }
 
     Box(Modifier.fillMaxSize()) {
-        if (showWhiteboard) {
+        if (whiteboardShown) {
             Whiteboard(
                 modifier = Modifier.fillMaxSize(),
-                whiteboardState = whiteboardState
+                state = whiteboardState,
             )
         }
 
-        OutlinedButton(
-            onClick = { showWhiteboard = !showWhiteboard },
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 32.dp)
-        ) {
-            Text(text = if (showWhiteboard) "Dismiss Whiteboard" else "Show Whiteboard")
+        if (!whiteboardShown) {
+            Text(
+                text = "Whiteboard Hidden",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(bottom = 32.dp)
+            )
         }
 
-        LazyRow(Modifier.padding(top = 32.dp)) {
-            item {
-                OperationButton("Pencil") {
-                    val memberState = MemberState().apply {
-                        currentApplianceName = "pencil"
-                    }
-                    whiteboardState.setMemberState(memberState)
+        FlowRow(Modifier.fillMaxWidth()) {
+            OperationButton("ShowOrHide") {
+                coroutineScope.launch {
+                    whiteboardShown = !whiteboardShown
                 }
+            }
 
-                OperationButton("Eraser") {
-                    val memberState = MemberState().apply {
-                        currentApplianceName = "eraser"
-                    }
-                    whiteboardState.setMemberState(memberState)
+            OperationButton("Pencil") {
+                // 更新教具状态
+                val memberState = MemberState().apply {
+                    currentApplianceName = "pencil"
                 }
+                whiteboardState.setMemberState(memberState)
+            }
 
-                OperationButton("Clear") {
-                    whiteboardState.clearScene()
+            OperationButton("Eraser") {
+                val memberState = MemberState().apply {
+                    currentApplianceName = "eraser"
                 }
+                whiteboardState.setMemberState(memberState)
+            }
 
-                OperationButton("RoomState") {
-                    coroutineScope.launch {
-                        val roomState = whiteboardState.getRoomState()
-                        println("RoomState: $roomState")
-                    }
+            OperationButton("Clear") {
+                // 清空白板
+                whiteboardState.clearScene()
+            }
+
+            OperationButton("RoomState") {
+                coroutineScope.launch {
+                    val roomState = whiteboardState.getRoomState()
+                    println("RoomState: $roomState")
+                }
+            }
+
+            // 非核心场景，测试 Whiteboard 变更使用
+            OperationButton("SwitchTest") {
+                coroutineScope.launch {
+                    whiteboardState.whiteboardProperties = WhiteboardProperties(
+                        appId = Constants.SAMPLE_APP_ID,
+                        roomUuid = Constants.SAMPLE_ROOM_UUID,
+                        roomToken = Constants.SAMPLE_ROOM_TOKEN,
+                        uniqueUid = Constants.SAMPLE_UNIQUE_UID + Random.nextInt(),
+                        region = Constants.SAMPLE_REGION,
+                    )
                 }
             }
         }
--- WhiteboardCompose/app/src/main/java/io/agora/board/samples/Whiteboard.kt
+++ WhiteboardCompose/app/src/main/java/io/agora/board/samples/Whiteboard.kt
@@ -1,17 +1,21 @@
 package io.agora.board.samples
 
+import android.content.Context
 import android.view.ViewGroup
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import com.herewhite.sdk.CommonCallback
 import com.herewhite.sdk.Room
 import com.herewhite.sdk.RoomListener
 import com.herewhite.sdk.RoomParams
@@ -20,29 +24,46 @@ import com.herewhite.sdk.WhiteSdkConfiguration
 import com.herewhite.sdk.WhiteboardView
 import com.herewhite.sdk.domain.MemberState
 import com.herewhite.sdk.domain.Promise
+import com.herewhite.sdk.domain.Region
 import com.herewhite.sdk.domain.RoomPhase
 import com.herewhite.sdk.domain.RoomState
 import com.herewhite.sdk.domain.SDKError
+import org.json.JSONObject
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
+@Immutable
+data class WhiteboardProperties(
+    val appId: String,
+    val roomUuid: String,
+    val roomToken: String,
+    val uniqueUid: String,
+    val region: Region,
+    val writable: Boolean = true
+) {
+    companion object {
+        val UNINITIALIZED = WhiteboardProperties("", "", "", "", Region.cn)
+    }
+}
+
 @Composable
 fun rememberWhiteboardState(
-    key: String? = null,
-    init: WhiteboardState.() -> Unit = {},
-): WhiteboardState = rememberSaveable(key = key, saver = WhiteboardState.Saver()) {
-    WhiteboardState().apply(init)
+    properties: WhiteboardProperties = WhiteboardProperties.UNINITIALIZED
+): WhiteboardState = rememberSaveable(saver = WhiteboardState.Saver()) {
+    WhiteboardState(properties)
 }
 
 @Stable
-class WhiteboardState {
+class WhiteboardState(properties: WhiteboardProperties) {
 
-    private var room by mutableStateOf<Room?>(null)
+    var whiteboardProperties: WhiteboardProperties by mutableStateOf(properties)
 
-    internal fun initRoom(r: Room) {
-        room = r
-    }
+    internal var whiteboardView by mutableStateOf<WhiteboardView?>(null)
+
+    internal var room by mutableStateOf<Room?>(null)
+
+    fun uninitialized() = whiteboardProperties == WhiteboardProperties.UNINITIALIZED
 
     suspend fun getRoomState(): RoomState? = suspendCoroutine { cont ->
         room?.getRoomState(object : Promise<RoomState> {
@@ -81,7 +102,6 @@ class WhiteboardState {
             override fun catchEx(t: SDKError) {
                 cont.resumeWithException(t)
             }
-
         })
     }
 
@@ -89,11 +109,36 @@ class WhiteboardState {
         room?.cleanScene(true)
     }
 
+    fun release() {
+        whiteboardView = null
+        room = null
+    }
+
     companion object {
-        fun Saver(): Saver<WhiteboardState, String> =
-            Saver(
-                save = { "" },
-                restore = { WhiteboardState() }
+        fun Saver(): Saver<WhiteboardState, Any> =
+            listSaver(
+                save = {
+                    listOf(
+                        it.whiteboardProperties.appId,
+                        it.whiteboardProperties.roomUuid,
+                        it.whiteboardProperties.roomToken,
+                        it.whiteboardProperties.uniqueUid,
+                        it.whiteboardProperties.region,
+                        it.whiteboardProperties.writable
+                    )
+                },
+                restore = {
+                    WhiteboardState(
+                        WhiteboardProperties(
+                            it[0] as String,
+                            it[1] as String,
+                            it[2] as String,
+                            it[3] as String,
+                            it[4] as Region,
+                            it[5] as Boolean
+                        )
+                    )
+                }
             )
     }
 }
@@ -101,35 +146,50 @@ class WhiteboardState {
 @Composable
 fun Whiteboard(
     modifier: Modifier,
-    whiteboardState: WhiteboardState = rememberWhiteboardState(),
+    state: WhiteboardState,
     onPhaseChanged: ((phase: RoomPhase) -> Unit)? = null,
     onRoomStateChanged: ((modifyState: RoomState) -> Unit)? = null,
-    onError: ((error: SDKError) -> Unit)? = null
+    onError: ((error: SDKError) -> Unit)? = null,
+    onCreated: (WhiteboardView) -> Unit = {},
+    onDispose: (WhiteboardView) -> Unit = {},
+    factory: ((Context) -> WhiteboardView)? = null,
 ) {
 
-    var whiteboardView by remember { mutableStateOf<WhiteboardView?>(null) }
+    val wv = state.whiteboardView
+    wv?.let {
+        LaunchedEffect(wv, state) {
+            snapshotFlow { state.whiteboardProperties }.collect {
+                println("state.whiteboardProperties $it")
 
-    DisposableEffect(
-        AndroidView(
-            factory = { ctx ->
-                whiteboardView = WhiteboardView(ctx).apply {
-                    layoutParams = ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT
-                    )
+                val properties = state.whiteboardProperties
+                if (properties == WhiteboardProperties.UNINITIALIZED)
+                    return@collect
+
+                val whiteSdkConfiguration = WhiteSdkConfiguration(properties.appId, true)
+                val whiteSdk = WhiteSdk(wv, wv.context, whiteSdkConfiguration).apply {
+                    setCommonCallbacks(object : CommonCallback {
+
+                        override fun onLogger(log: JSONObject) {
+
+                        }
+
+                        override fun sdkSetupFail(error: SDKError) {
+                            onError?.invoke(error)
+                        }
+
+                        override fun throwError(args: Any) {
+                            onError?.invoke(SDKError("Uncaught sdk exception"))
+                        }
+                    })
                 }
 
-                val whiteSdkConfiguration = WhiteSdkConfiguration(Constants.SAMPLE_APP_ID, true)
-                val whiteSdk = WhiteSdk(whiteboardView, ctx, whiteSdkConfiguration)
-
                 val roomParams = RoomParams(
-                    /* uuid = */ Constants.SAMPLE_ROOM_UUID,
-                    /* token = */ Constants.SAMPLE_ROOM_TOKEN,
-                    /* uid = */ Constants.SAMPLE_UNIQUE_UID,
+                    properties.roomUuid,
+                    properties.roomToken,
+                    properties.uniqueUid
                 ).apply {
-                    // 设置可写
-                    isWritable = true
-                    region = Constants.SAMPLE_REGION
+                    isWritable = properties.writable
+                    region = properties.region
                 }
 
                 whiteSdk.joinRoom(roomParams, object : RoomListener {
@@ -163,8 +223,7 @@ fun Whiteboard(
 
                 }, object : Promise<Room> {
                     override fun then(room: Room) {
-                        whiteboardState.initRoom(room)
-
+                        state.room = room
                         // 开启本地序列化, 触发 onCanUndoStepsUpdate, onCanRedoStepsUpdate 回调
                         room.disableSerialization(false)
                         // 禁止客户端视角操作
@@ -176,19 +235,31 @@ fun Whiteboard(
                     }
                 }
                 )
-
-                return@AndroidView whiteboardView!!
-            },
-
-            modifier = modifier
-        )
-    ) {
-        onDispose {
-            whiteboardView?.run {
-                removeAllViews()
-                destroy()
-                whiteboardView = null
             }
         }
     }
+
+    AndroidView(
+        factory = { context ->
+            (factory?.invoke(context) ?: WhiteboardView(context)).apply {
+                onCreated(this)
+
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+
+            }.also { state.whiteboardView = it }
+        },
+        modifier = modifier,
+        update = {
+            print("Whiteboard update call")
+        },
+        onRelease = {
+            onDispose(it)
+            it.removeAllViews()
+            it.destroy()
+            state.release()
+        }
+    )
 }
